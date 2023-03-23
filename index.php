f5e1030f4997cc3dcf8a5eb987ce5058f1c9cff9
Tharindu <tharinduprabashwara71@gmail.com>
2023-03-23 19:35:35 +0530

    new_helpcenter-with-main

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
           <input type=button onClick="parent.location='./loginStaff.php'" class="homebutton" value='Staff Login'>
           <br />
           <input type=button onClick="parent.location='./customerLogin.php'" class="homebutton" style="background-color: gray;" value='Customer'>
-          <a href="./help/helpcenter.php" class="help"><i class="fa-sharp fa-solid fa-circle-info "></i>&nbsp;Need Help?</a><br>
+          <a href="./help/helpcenter.html" class="help"><i class="fa-sharp fa-solid fa-circle-info "></i>&nbsp;Need Help?</a><br>
           &nbsp; &nbsp; &nbsp;<a href="./Roles.html" class="help"><i class="fa fa-tasks" aria-hidden="true"></i>
 &nbsp;Functionalities</a>
           
@@ -35,5 +35,4 @@
     </div>
   </div>
 </body>
-
 </html>
